6286ea20b0094d86f4086b3555a0f5ce13962041
Alex Chescheir <alexchescheir@Nancys-MacBook-Pro.local>
2016-04-03 12:19:07 -0400

    Started on evil version. It has the UI in place but there are bugs in the executuin

---
+++ evil_mystery_word.py
@@ -0,0 +1,88 @@
+import mystery_word as mw
+from collections import Counter
+
+def get_signature(candidate_word, guesses):
+    signature = []
+    for each_letter in candidate_word:
+        if each_letter in guesses:
+            signature.append(each_letter)
+        else:
+            signature.append('_')
+    return ''.join(signature)
+
+def get_all_signatures(candidate_word_list,guesses):
+    signatures = {}
+    for each_word in candidate_word_list:
+            signature = get_signature(each_word, guesses)
+            if signature in signatures:
+                signatures[signature].append(each_word)
+            else:
+                signatures[signature] = [each_word]
+    return signatures
+
+def evaluate_guess(candidate_word_list,guesses):
+    signatures = get_all_signatures(candidate_word_list,guesses)
+    count = 0
+    signature = ''
+    for key in signatures.keys():
+        if len(signatures[key]) >= count:
+            count = len(signatures[key])
+            signature = key
+    return signature
+
+def get_candidates(candidate_word_list,guesses,signature):
+    return get_all_signatures(candidate_word_list,guesses)[signature]
+
+def display_status(guesses, signature):
+    print(20*'=')
+    print("Guess #{}".format(len(guesses)))
+    print("Word: "+signature)
+    print("Misses: {}".format([x for x in guesses if x not in signature]))
+
+def play_a_turn(candidate_word_list,guesses, max_misses):
+    display_status(guesses, evaluate_guess(candidate_word_list,guesses))
+    guess = input("> ")
+    guesses.append(guess)
+    signature = evaluate_guess(candidate_word_list,guesses)
+    candidate_word_list = get_candidates(candidate_word_list,guesses,signature)
+    #display status
+    #prompr for guess
+    #evaluate guesses
+
+
+def is_round_over(candidate_word_list, guesses, max_misses):
+    if len(candidate_word_list) ==0:
+        return True
+    word_signature = evaluate_guess(candidate_word_list,guesses)
+    if "_" not in word_signature:
+        return True
+    misses = [x for x in guesses if x not in word_signature]
+    if len(misses) >max_misses:
+        return True
+    return False
+
+def play_game_round(candidate_word_list):
+    max_misses = 13
+    length = 4
+    candidate_word_list = [x for x in candidate_word_list if len(x) == length]
+    guesses = []
+    while True:
+        if is_round_over(candidate_word_list, guesses, max_misses):
+            # finish_round(secret_word, guesses, max_misses)
+            break
+        else:
+            play_a_turn(candidate_word_list, guesses, max_misses)
+
+
+def main():
+    word_list = mw.read_dictionary()
+
+    while True:
+        play_game_round([x.lower() for x in word_list])
+        if not mw.should_play_again():
+            print("Thanks for playing.")
+            break
+    print("Goodbye.")
+
+if __name__ == '__main__':
+    main()
---
+++ evil_mystery_word_test.py
@@ -0,0 +1,44 @@
+import unittest
+import evil_mystery_word as emw
+
+class TestMysteryWord(unittest.TestCase):
+    #
+    # def test_assert_true(self):
+    #     self.assertTrue(True)
+    #
+    # def test_assert_false(self):
+    #     self.assertFalse(False)
+    #
+    # def test_assert_equal(self):
+    #     self.assertEqual(1, 1)
+    #
+    # def test_assert_not_equal(self):
+    #     self.assertNotEqual(1, 0)
+    def test_get_signature_empty(self):
+        self.assertEqual(emw.get_signature('battleship',[]),'__________')
+
+    def test_get_signature_partial(self):
+        self.assertEqual(emw.get_signature('battleship',['b','a','t','h']),'batt___h__')
+
+    def test_get_signature_full(self):
+        self.assertEqual(emw.get_signature('battleship',[x for x in 'battleship']),'battleship')
+
+    def test_get_signature_mixed(self):
+        self.assertEqual(emw.get_signature('battleship',['b','x','t','h']),'b_tt___h__')
+
+    def test_count_signatures(self):
+        self.assertEqual(emw.get_all_signatures(['bat','cat','bam'],['a','t']),{'_at':['bat','cat'],'_a_':['bam']})
+
+    def test_evaluate_guess(self):
+        test_list =['bat','cat','bam']
+        test_guesses = ['a','t']
+        self.assertEqual(emw.evaluate_guess(test_list,test_guesses),'_at')
+
+    def test_get_candidates(self):
+        test_list =['bat','cat','bam']
+        test_guesses = ['a','t']
+        self.assertEqual(emw.get_candidates(test_list,test_guesses,'_at'),['bat','cat'])
+        self.assertTrue(test_list == ['bat','cat','bam'])
+
+if __name__ == '__main__':
+    unittest.main()
